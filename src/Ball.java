97b742eda29944937b5bd44802becba018d79d8c
Thomas Edwards <tomo12321@hotmail.com>
2017-06-11 17:40:42 +1200

    first commit

---
+++ src/Ball.java
@@ -0,0 +1,112 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.List;
+
+public class Ball {
+
+	double mass;
+	double xVelocity, yVelocity;
+	double radius;
+	double x, oldX, oldY, y;// these are the centres of the circles
+	Color col;
+
+	/**
+	 * Constructs a ball with the specified properties
+	 */
+	public Ball(double mass, double radius, double x, double y, double xSpeed, double ySpeed, Color colour) {
+		this.mass = mass;
+		this.radius = radius;
+		this.x = x;
+		this.y = y;
+		this.xVelocity = xSpeed;
+		this.yVelocity = ySpeed;
+		this.col = colour;
+	}
+
+	/**
+	 * Constructs a Ball which has random properties within a certain range. The ball constructed is guaranteed to not
+	 * overlap with any of the balls in the balls List.
+	 * 
+	 * @param balls
+	 *            the list of balls which have already been added
+	 */
+	public Ball(List<Ball> balls) {
+		this.mass = Math.random() * 50;
+		this.radius = Math.random() * 20 + 10;
+		this.xVelocity = Math.random() * 400 + 50;
+		this.yVelocity = Math.random() * 400 + 50;
+		this.col = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+		boolean algood = false;
+		while (!algood) {// ensures that a ball isnt overlapping with another ball
+			this.x = Math.random() * 200 + 70 + radius / 2.2;
+			this.y = Math.random() * 200 + 70 + radius / 2.2;
+			int i = 0;
+			for (; i < balls.size(); i++) {
+				Ball b = balls.get(i);
+				if (touchingCircles(x, y, radius, b.x, b.y, b.radius)) {
+					i--;
+					break;
+				}
+			}
+			if (i == balls.size())
+				algood = true;
+		}
+	}
+
+	/**
+	 * Called every 0.003 seconds, moves the position of the balls 0.001 of their speed in the correct direction
+	 */
+	public void move() {
+		double yMovement = yVelocity * 0.001;
+		double xMovement = xVelocity * 0.001;
+		oldX = x;
+		oldY = y;
+		x += xMovement;
+		y += yMovement;
+	}
+
+	/**
+	 * Checks whether this ball is touching the other ball, if it is then it changes the celocities of both balls to
+	 * simulate the collision.
+	 * 
+	 * @param b
+	 *            the other ball
+	 */
+	public void compareBalls(Ball b) {
+		// if the balls touch, we have a collision
+		if (this.touchingCircles(this.x, this.y, this.radius, b.x, b.y, b.radius)) {
+			double ballxVel1 = (this.xVelocity * (this.mass - b.mass) + 2 * b.mass * b.xVelocity)
+					/ (this.mass + b.mass);
+			double ballxVel2 = (b.xVelocity * (b.mass - this.mass) + 2 * this.mass * this.xVelocity)
+					/ (this.mass + b.mass);
+
+			double ballyVel1 = (this.yVelocity * (this.mass - b.mass) + 2 * b.mass * b.yVelocity)
+					/ (this.mass + b.mass);
+			double ballyVel2 = (b.yVelocity * (b.mass - this.mass) + 2 * this.mass * this.yVelocity)
+					/ (this.mass + b.mass);
+
+			this.xVelocity = ballxVel1;
+			this.yVelocity = ballyVel1;
+			b.xVelocity = ballxVel2;
+			b.yVelocity = ballyVel2;
+
+		}
+	}
+
+	/**
+	 * Draws the ball the graphics pane
+	 * 
+	 * @param g
+	 */
+	public void drawNew(Graphics g) {
+		g.setColor(col);
+		g.fillOval((int) (x - radius), (int) (y - radius), (int) (radius * 2), (int) (radius * 2));
+	}
+
+	private boolean touchingCircles(double x1, double y1, double rad1, double x2, double y2, double rad2) {
+		double xDistance = Math.abs(x1 - x2);// x distance between centers
+		double yDistance = Math.abs(y1 - y2);// y distance between centers
+		return (Math.hypot(xDistance, yDistance) <= rad1 + rad2);
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,165 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ * Bouncing balls :)
+ * 
+ * @author tomo1_000
+ *
+ */
+public class Main {
+
+	private final int boundaryX = 50;
+	private final int boundaryY = 50;
+	private final int boundaryWidth = 400;
+	private final int boundaryHeight = 400;
+
+	private List<Ball> balls = new ArrayList<Ball>();
+
+	public Main() {
+		addBalls();
+		JComponent panel = setupGUI();// this panel is what contains the paint component
+		while (true) {
+			panel.repaint();
+			this.update();
+			this.checkCollisions();
+		}
+	}
+
+	/**
+	 * setups the gui.
+	 * 
+	 * @return the JComponent which is responsible for drawing the balls.
+	 */
+	public JComponent setupGUI() {
+		JFrame frame = new JFrame();
+		JComponent panel = new JComponent() {
+			protected void paintComponent(Graphics g) {
+				g.drawRect(boundaryX - 2, boundaryY - 2, boundaryWidth + 3, boundaryHeight + 3);
+				g.clearRect(boundaryX - 1, boundaryY - 1, boundaryWidth + 2, boundaryHeight + 2);
+				for (Ball b : balls) {
+					b.drawNew(g);
+				}
+			}
+		};
+		frame.setSize(boundaryX * 3 + boundaryWidth, boundaryY * 3 + boundaryHeight);
+		frame.setTitle("Bouncing Balls");
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		return panel;
+	}
+
+	/**
+	 * Adds balls, 33% to add totally random balls. 33% chance to add varied balls, 33% chance to add black and red
+	 * balls.
+	 */
+	public void addBalls() {
+		double rand = Math.random();
+		if (rand < 0.33)
+			addRandomBalls((int) (Math.random() * 15));
+		else if (rand < 0.66)
+			addVariedBalls();
+		else
+			addEvenBalls();
+	}
+
+	public void addRandomBalls(int amount) {
+		for (int i = 0; i < amount; i++) {
+			Ball b1 = new Ball(balls);
+			balls.add(b1);
+		}
+	}
+
+	public void addVariedBalls() {
+		Ball b1 = new Ball(15, 10, 295, 230, 50, 300, Color.BLUE);
+		balls.add(b1);
+		Ball b2 = new Ball(20, 15, 250, 250, 300, 40, Color.RED);
+		balls.add(b2);
+		Ball b3 = new Ball(30, 8, 100, 150, 200, 300, Color.GREEN);
+		balls.add(b3);
+		Ball b4 = new Ball(40, 15, 100, 250, 400, 600, Color.pink);
+		balls.add(b4);
+		Ball b5 = new Ball(50, 20, 80, 90, -300, 0, Color.YELLOW);
+		balls.add(b5);
+		Ball b6 = new Ball(200, 20, 200, 90, 300, 0, Color.black);
+		balls.add(b6);
+		Ball b7 = new Ball(150, 10, 195, 230, -400, -300, Color.orange);
+		balls.add(b7);
+	}
+
+	public void addEvenBalls() {
+		Ball b1 = new Ball(10, 15, 295, 230, 50, 300, Color.BLACK);
+		balls.add(b1);
+		Ball b2 = new Ball(10, 15, 250, 250, 300, 540, Color.RED);
+		balls.add(b2);
+		Ball b3 = new Ball(10, 15, 100, 150, 200, 300, Color.BLACK);
+		balls.add(b3);
+		Ball b4 = new Ball(10, 15, 100, 250, 400, 600, Color.RED);
+		balls.add(b4);
+		Ball b5 = new Ball(10, 15, 80, 90, -300, 650, Color.BLACK);
+		balls.add(b5);
+		Ball b6 = new Ball(10, 15, 200, 90, 300, -450, Color.RED);
+		balls.add(b6);
+		Ball b7 = new Ball(10, 15, 195, 230, -600, -300, Color.BLACK);
+		balls.add(b7);
+		Ball b8 = new Ball(10, 15, 300, 190, -300, -450, Color.RED);
+		balls.add(b8);
+		Ball b9 = new Ball(10, 15, 300, 90, -300, 50, Color.BLACK);
+		balls.add(b9);
+		Ball b10 = new Ball(10, 15, 200, 400, 300, -550, Color.RED);
+		balls.add(b10);
+		Ball b11 = new Ball(10, 15, 95, 330, -400, -300, Color.BLACK);
+		balls.add(b11);
+		Ball b12 = new Ball(10, 15, 100, 400, 700, -450, Color.RED);
+		balls.add(b12);
+	}
+
+	/**
+	 * Sleeps for 0.003 seconds after updating
+	 */
+	public void update() {
+		for (Ball b : balls) {
+			b.move();
+		}
+		try {
+			Thread.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void checkCollisions() {
+		for (int i = 0; i < balls.size(); i++) {
+			Ball b1 = balls.get(i);
+			this.touchingTopBotWalls(b1);
+			this.touchingLeftRightWalls(b1);
+			for (int j = i + 1; j < balls.size(); j++) {
+				Ball b2 = balls.get(j);
+				b1.compareBalls(b2);
+			}
+		}
+	}
+
+	public void touchingTopBotWalls(Ball b) {
+		if (b.y + b.radius >= boundaryHeight + boundaryY - 1 || b.y - b.radius <= boundaryY + 1) {
+			b.yVelocity = -b.yVelocity;
+		}
+	}
+
+	public void touchingLeftRightWalls(Ball b) {
+		if (b.x - b.radius <= boundaryX + 1 || b.x + b.radius >= boundaryX + boundaryWidth - 1) {
+			b.xVelocity = -b.xVelocity;
+		}
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
